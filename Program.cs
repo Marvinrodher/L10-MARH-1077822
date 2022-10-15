e36010d17ce9e81b64d96fcd6fb3ba40c9114be1
Alexander <Alexander@DESKTOP-7GV22OO>
2022-10-14 18:07:25 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace L10_MARH_1077822
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lab 9");
+            Console.WriteLine("Ingrese su usuario: ");
+            for (int x = 0; x < 3; x++)
+            {
+                string contraseña = Convert.ToString(Console.ReadLine());
+                if (contraseña == "usuario1")
+                {
+                    Console.WriteLine("verdad");
+                }
+            }
+            Console.WriteLine("Ingrese su contraseña: ");
+            for (int x = 0; x < 3; x++)
+            {
+                string contraseña = Convert.ToString(Console.ReadLine());
+                if (contraseña == "asdasd")
+                {
+                    Console.WriteLine("verdad");
+                }
+            }
+        }
+    }
+}       
